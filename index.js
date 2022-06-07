a2e4fe1f2a0f6af167025e1b15f431618bf86c27
geraldosalazar16 <geraldosalazar16@gmail.com>
2022-06-07 15:44:45 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+console.log(new Date('Jun 7, 2022 6:16:16 PM UTC').toString());
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
